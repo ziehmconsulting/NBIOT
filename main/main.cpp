a02f30275fe1a55c3f7c4dd6b684add5880a3c3a
TjarkZiehm <tjark@ziehmconsulting.com>
2024-10-15 18:16:33 +0200

    init upload

---
+++ main/main.cpp
@@ -0,0 +1,24 @@
+//file: main.cpp
+#include "Arduino.h"
+
+#ifdef ARDUINO_ARCH_ESP32
+#include "esp32-hal-log.h"
+#endif
+
+extern "C" void app_main()
+{
+  initArduino();
+
+  // Arduino-like setup()
+  Serial.begin(115200);
+  while(!Serial){
+    ; // wait for serial port to connect
+  }
+
+  // Arduino-like loop()
+  while(true){
+    Serial.println("loop");
+  }
+
+  // WARNING: if program reaches end of function app_main() the MCU will restart.
+}
